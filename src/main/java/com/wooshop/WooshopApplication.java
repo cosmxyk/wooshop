fe139ddf656d1924e6d89c7b2a98b93867ff3fa2
wooyoung <uyfly@naver.com>
2026-04-24 00:10:43 +0900

    Initial commit

---
+++ src/main/java/com/wooshop/WooshopApplication.java
@@ -0,0 +1,13 @@
+package com.wooshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooshopApplication.class, args);
+	}
+
+}
